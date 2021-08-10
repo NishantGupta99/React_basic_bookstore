81ad9ecda4220bf6ee4925e89b6e62460c4c9749
nishant Gupta <kr.nish99@gmail.com>
2021-08-10 14:49:44 +0530

    we added css styling using jsx into our Author function

--- src/Bookstore.js
+++ src/Bookstore.js
@@ -22,5 +22,5 @@ const Book = () => {
 }
 const Img = () => <img src="https://images-eu.ssl-images-amazon.com/images/I/711hfHGE%2BcL._AC_UL200_SR200,200_.jpg" alt=""></img>
 const Title = () => <h1>Great Stories for Children </h1>
-const Author = () => <h3>Ruskin Bond </h3>
+const Author = () => <h3 style={{color:'#617d98', fontSize:'0.75rem', marginTop:"0.25rem" }} >Ruskin Bond </h3>
 export default Bookstore

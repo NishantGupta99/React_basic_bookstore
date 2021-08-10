b904737da3476fc09e3a0f3e95f151b157ddd116
nishant Gupta <kr.nish99@gmail.com>
2021-08-10 13:06:49 +0530

    we added css to our bookstore and made it a lil stylish

--- src/Bookstore.js
+++ src/Bookstore.js
@@ -1,16 +1,26 @@
 import React from 'react'
+import './index.css'
 
 function Bookstore() {
     return (
-       <section>
+       <section className="booklist">
+         <Book /> 
+         <Book /> 
+         <Book /> 
+         <Book /> 
          <Book />         
        </section>
     )
 }
 
 const Book = () => {
-    return <article><Img></Img></article>
+    return <article>
+        <Img />
+        <Title/>
+        <Author/>
+         </article>
 }
 const Img = () => <img src="https://images-eu.ssl-images-amazon.com/images/I/711hfHGE%2BcL._AC_UL200_SR200,200_.jpg" alt=""></img>
-
+const Title = () => <h1>Great Stories for Children </h1>
+const Author = () => <h3>Ruskin Bond </h3>
 export default Bookstore

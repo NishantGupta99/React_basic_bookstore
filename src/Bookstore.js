560805b2f38fcd3ce47584381f306c1b40890bc4
nishant Gupta <kr.nish99@gmail.com>
2021-08-10 15:56:23 +0530

    we learned how to use props and different ways of using props using js destructuring

--- src/Bookstore.js
+++ src/Bookstore.js
@@ -1,26 +1,45 @@
 import React from 'react'
 import './index.css'
 
+const FirstBook = {
+    title: "Great Stories for Children ",
+    author: "Ruskin Bond",
+    img: "https://images-eu.ssl-images-amazon.com/images/I/711hfHGE%2BcL._AC_UL200_SR200,200_.jpg"
+
+}
+const SecondBook = {
+    title: "Word Power Made Easy",
+    author: "Norman Lewis",
+    img: "https://images-eu.ssl-images-amazon.com/images/I/818e%2Bfq7%2BBL._AC_UL200_SR200,200_.jpg"
+
+}
+const ThirdBook = {
+    title: "Think Like a Monk",
+    author: "Jay Shetty",
+    img: "https://images-eu.ssl-images-amazon.com/images/I/81s6DUyQCZL._AC_UL200_SR200,200_.jpg",
+}
+
 function Bookstore() {
     return (
-       <section className="booklist">
-         <Book /> 
-         <Book /> 
-         <Book /> 
-         <Book /> 
-         <Book />         
-       </section>
+        <section className="booklist">
+            <Book img={FirstBook.img} title={FirstBook.title} author={FirstBook.author} />
+            {/*we have passed data in the object through the component using props*/}
+            <Book img={SecondBook.img} title={SecondBook.title} author={SecondBook.author}></Book>
+            <Book img={ThirdBook.img} title={ThirdBook.title} author={ThirdBook.author}></Book>
+
+        </section>
     )
 }
 
-const Book = () => {
-    return <article>
-        <Img />
-        <Title/>
-        <Author/>
-         </article>
+const Book = ({img,title,author}) => {
+   // const {img, title,author} = props; // this is just another way to use props and depends upto the programmer 
+
+    return (<article className='book'>
+        <img src={img}></img>
+        <h1>{title}</h1>
+        <h3>{author}</h3>
+    </article>
+    );
 }
-const Img = () => <img src="https://images-eu.ssl-images-amazon.com/images/I/711hfHGE%2BcL._AC_UL200_SR200,200_.jpg" alt=""></img>
-const Title = () => <h1>Great Stories for Children </h1>
-const Author = () => <h3 style={{color:'#617d98', fontSize:'0.75rem', marginTop:"0.25rem" }} >Ruskin Bond </h3>
+
 export default Bookstore
